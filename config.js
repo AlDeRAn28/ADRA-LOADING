1749602db6252958beb43fd1aa802c04545147b2
AlDeRAn <baran28@naver.com>
2019-05-11 21:46:21 +0900

    add

---
+++ config.js
@@ -0,0 +1,59 @@
+﻿// ADRA 로딩화면 - 기본 버전
+//
+// ⓒ 2019. AlDeRAn All rights reserved.
+// 저작자: AlDeRAn(https://steamcommunity.com/profiles/76561198128012369/)
+
+//로고 설정
+// images\logo 폴더에 이미지를 넣어주고 아래 파일명과 확장자를 적어주세요.
+// 비활성화 방법: "" 공백 유지.
+var logo = "logo.png";
+
+// 음악 또는 영상 중 어떤 방법으로 재생할지 선택합니다.
+// "v" 영상을 재생합니다.
+// "a" 음악을 재생합니다.
+// [기본 버전 미제공] ""(공백) 음악 또는 영상 중 랜덤으로 재생합니다.
+var play_type = "a";
+
+// 영상 리스트
+// 영상은 webm 확장자만 지원합니다. (게리모드는 MP4를 거부합니다 ㅡㅅㅡ)
+// 음악 리스트와 달리 영상 리스트는 기본값 .webm 로 정의 되어있어 파일 이름만 적어주시면 됩니다.
+var video_list = [
+    "video"
+];
+
+// 영상 볼륨
+// 0에서 1사이 값
+var video_volume = 0.2;
+
+// 음악 리스트
+// [기본 버전 미제공] EX. {file: "filename.ogg", name: "Song Name"}
+// EX. "filename.ogg"
+// 다중 추가시 끝에 ,(반점)을 추가하셔야 합니다. (마지막 줄은 해도 안해도 상관없지만 안하는 것이 원칙입니다.)
+var audio_list = [
+    "Diamond Eyes - Flutter.mp3",
+    "Lost Sky - Dreams.mp3",
+    "Tobu - Roots _NCS Release_.mp3",
+    "Afterlife Ticket.ogg"
+];
+
+// 음악 볼륨
+// 0에서 1사이 값
+var audio_volume = 0.15;
+
+// 사진 리스트
+// 다중 추가시 끝에 ,(반점)을 추가하셔야 합니다. (마지막 줄은 해도 안해도 상관없지만 안하는 것이 원칙입니다.)
+var img_list = [
+    "background-biker-clouds-207171.jpg",
+    "beach-dawn-desktop-backgrounds-1532771.jpg",
+    "clouds-dawn-desktop-backgrounds-46253.jpg",
+    "fall-colors-fall-foliage-forest-1563355.jpg",
+    "asphalt-back-road-blurred-background-1546901.jpg"
+];
+
+// 사진 변경 시간
+// 1000 = 1초
+var img_duration = 7000;
+
+// 사진 전환 시간
+// 1000 = 1초
+var img_fade = 3000;
---
+++ js/adra.js
@@ -0,0 +1,92 @@
+﻿// ADRA 로딩화면 - 기본 버전
+//
+// ⓒ 2019. AlDeRAn All rights reserved.
+// 저작자: AlDeRAn(https://steamcommunity.com/profiles/76561198128012369/)
+function SetStatusChanged ( status ) {
+    if (status == "Starting Lua...") return $('status').html("다운로드 완료.. 접속시도중..");
+    $('status').html(status);
+}
+
+var audio, video;
+var a_i = 0, v_i = 0;
+
+$(function() {
+    $("body").attr('onload', 'onbody()');
+    $("body").append("<audio id=\"l_audio\" onended=\"on_a_embed()\"></audio>");
+    $("body").append("<video id=\"l_video\" onended=\"on_v_embed()\"></video>");
+    $("body").append("<div id=\"overlay\"></div>");
+    $("body").append("<div id=\"logo\"><img></div>");
+});
+
+function onbody() {
+    if (play_type == "a") {
+        img();
+        audio = document.getElementById('l_audio');
+        audio_list = random(audio_list);
+        audio.setAttribute("src", "music/"+audio_list[a_i]);
+        audio.play();
+        audio.volume = audio_volume;
+    } else if (play_type == "v") {
+        video = document.getElementById('l_video');
+        video_list = random(video_list);
+        video.setAttribute("src", "video/"+video_list[v_i]+".webm");
+        video.play();
+        video.volume = video_volume;
+    } else {
+        play_type = 'a';
+        onbody()
+    }
+
+    if (logo && logo != "") {
+        $('#logo img').attr('src', "images/logo/"+logo);
+    }
+}
+
+function on_a_embed() {
+    if (a_i < audio_list.length - 1) {
+        a_i++;
+    } else {
+        a_i = 0;
+    }
+    audio.setAttribute("src", "music/"+audio_list[a_i]);
+    audio.play();
+    audio.volume = audio_volume;
+}
+
+function on_v_embed() {
+    if (v_i < video_list.length - 1) {
+        v_i++;
+    } else {
+        v_i = 0;
+    }
+    video.setAttribute("src", "video/"+video_list[v_i]+".webm");
+    video.play();
+    video.volume = video_volume;
+}
+
+// 배열 랜덤 알고리즘
+function random(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+}
+
+function img() {
+    for (var i = 0; i < img_list.length; i++) {
+        img_list[i] = 'images/' + img_list[i];
+    }
+    img_list = random(img_list);
+    $.backstretch(img_list, {fade: img_fade, duration: img_duration});
+}
